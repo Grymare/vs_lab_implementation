7f7da47b75091a6875c7b044f7f2f33177ce97a5
codebecker <chrisbeckerwb@gmail.com>
2020-12-03 11:51:51 +0100

    Added core service category

---
+++ core-service-category/src/main/java/de/hska/iwi/vslab/coreservicecategory/CoreServiceCategoryApplication.java
@@ -0,0 +1,15 @@
+package de.hska.iwi.vslab.coreservicecategory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CoreServiceCategoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreServiceCategoryApplication.class, args);
+	}
+
+}
