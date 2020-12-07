252857ea5e0062e99486e4f49f9bdcef868bbbe3
codebecker <chrisbeckerwb@gmail.com>
2020-12-07 13:36:45 +0100

    modified product database

--- database_product/init.sql
+++ database_product/init.sql
@@ -1,10 +1,12 @@
 CREATE TABLE public.product (
     id int PRIMARY KEY,
     name varchar(255),
-    price float,
-    category int
+    price double precision,
+    details varchar(255)
+    categoryId int
+
 );
 
-INSERT INTO product (name, price, category) VALUES ('club_mate', 1.3, 0);
-INSERT INTO product (name, price, category) VALUES ('apple', 0.3, 1);
-INSERT INTO product (name, price, category) VALUES ('t_shirt', 10.0, 2);
+INSERT INTO product (name, price, details ,categoryId) VALUES ('club_mate', 1.3, 'lecker und spritzig' , 0);
+INSERT INTO product (name, price, category) VALUES ('apple', 0.3, 'one per day', 1);
+INSERT INTO product (name, price, category) VALUES ('t_shirt', 10.0, 'made from happy sheeps', 2);
