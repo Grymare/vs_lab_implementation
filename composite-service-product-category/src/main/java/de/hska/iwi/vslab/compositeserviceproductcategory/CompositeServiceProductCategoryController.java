f5ecaf03fa72e1e9fd9c3c66c29c6a4a8b9d9ec1
codebecker <chrisbeckerwb@gmail.com>
2020-12-08 20:23:19 +0100

    added basic files for composite service

---
+++ composite-service-product-category/src/main/java/de/hska/iwi/vslab/compositeserviceproductcategory/CompositeServiceProductCategoryController.java
@@ -0,0 +1,83 @@
+package de.hska.iwi.vslab.compositeserviceproductcategory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CompositeServiceProductCategoryController {
+
+    @Autowired
+    /**
+     * Gibt alle Kategorien zurück die es in der Datenbank gibt
+     * Die Rückgabe erfolgt über eine Liste (müssen wir zu XML wandeln)
+     */
+
+    @RequestMapping(value="/category", method=RequestMethod.GET)
+        public ResponseEntity<Object> getCategories() {
+
+            //schauen wie wir aus der DB auslesen können
+            Iterable<Category> categoryIterable = categoryRepository.findAll(); 
+            
+            /*
+            List<Category> categoryList = new ArrayList<Category>();
+
+            for (Category cat : categoryIterable) {
+                categoryList.add(cat);
+            }*/
+
+            JSONArray json_array = new JSONArray();
+            for (Category cat : categoryIterable) {
+                json_array.put(cat.getJSONObject());
+            }
+            
+            System.out.println(json_array.toString());
+ 
+            return new ResponseEntity<Object>(json_array.toString(), HttpStatus.OK);
+        }
+
+    /**
+     * Erstellt ein neues Category-Objekt mit dem übermittelten Kategoriennamen
+     * 
+     * @param categoryName Name der neuen Kategorie
+     * @return gibt einen HTTP Status 200 zurück
+     */
+    @RequestMapping(value = "/category{categoryName}", method = RequestMethod.POST)
+    public HttpStatus postCategory(@PathVariable String categoryName) {
+
+        Category catego = new Category(categoryName);
+
+        categoryRepository.save(catego);
+        return HttpStatus.OK;
+    }
+
+    @RequestMapping(value = "/category/{categoryID}", method = RequestMethod.GET)
+    public ResponseEntity<Category> getCategory(@PathVariable Integer categoryID) {
+
+        Optional<Category> categoryOptional = categoryRepository.findById(categoryID);
+        
+        if (categoryOptional.isEmpty()){
+            return new ResponseEntity<Category>(HttpStatus.NOT_FOUND);              
+        }else{
+            return new ResponseEntity<Category>(categoryOptional.get(), HttpStatus.OK); 
+        }       
+        
+    }
+
+    @RequestMapping(value = "/category/{categoryID}", method = RequestMethod.DELETE)
+    public HttpStatus deleteCategory(@PathVariable Integer categoryID) {
+        categoryRepository.deleteById(categoryID);
+        return HttpStatus.OK;
+    }
+}
---
+++ composite-service-product-category/src/main/java/de/hska/iwi/vslab/compositeserviceproductcategory/Product.java
@@ -0,0 +1,83 @@
+package de.hska.iwi.vslab.coreserviceproduct;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.json.JSONObject;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	private String name;
+	private double price;
+	private String details;
+	private int categoryID;
+
+	
+	public Product() {
+	}
+
+	public Product(String name, double price, String details,  int categoryId) {
+		this.name = name;
+		this.price = price;
+		this.categoryID = categoryId;
+		this.details = details;
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return this.price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getCategoryId() {
+		return this.categoryID;
+	}
+
+	public void setCategory(int category) {
+		this.categoryID = category;
+	}
+
+	public String getDetails() {
+		return this.details;
+	}
+
+	public void setDetails(String details) {
+		this.details = details;
+	}
+
+	public JSONObject getJSONObject(){
+		JSONObject prod = new JSONObject();
+
+		prod.put("id", this.id);
+		prod.put("price", this.price);
+		prod.put("categoryID", this.categoryID);
+		prod.put("details", this.details);
+		return prod;
+	} 
+
+
+}
