d27ec198e7118ec365828257eda341dd8adb128f
codebecker <chrisbeckerwb@gmail.com>
2020-12-09 12:49:55 +0100

    minor changes

--- database_product/init.sql
+++ database_product/init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE public.product (
-    id int PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name varchar(255),
     price double precision,
     details varchar(255),
@@ -7,6 +7,6 @@ CREATE TABLE public.product (
 
 );
 
-INSERT INTO product (id, name, price, details ,categoryId) VALUES (0, 'club_mate', 1.3, 'lecker und spritzig' , 0);
-INSERT INTO product (id, name, price, details, categoryId) VALUES (1, 'apple', 0.3, 'one per day', 1);
-INSERT INTO product (id,name, price, details, categoryId) VALUES (2, 't_shirt', 10.0, 'made from happy sheeps', 2);
+INSERT INTO product (name, price, details ,categoryId) VALUES ('club_mate', 1.3, 'lecker und spritzig' , 1);
+INSERT INTO product (name, price, details, categoryId) VALUES ('apple', 0.3, 'one per day', 2);
+INSERT INTO product (name, price, details, categoryId) VALUES ('t_shirt', 10.0, 'made from happy sheeps', 3);
