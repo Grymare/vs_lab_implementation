994a3156393407559442f2b0499984bc89cf3942
codebecker <chrisbeckerwb@gmail.com>
2020-12-10 10:24:18 +0100

    try to run hystrix

--- core-service-category/src/main/java/de/hska/iwi/vslab/coreservicecategory/CoreServiceCategoryApplication.java
+++ core-service-category/src/main/java/de/hska/iwi/vslab/coreservicecategory/CoreServiceCategoryApplication.java
@@ -2,6 +2,7 @@ package de.hska.iwi.vslab.coreservicecategory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 @EnableHystrix
 @EnableHystrixDashboard
 @Configuration
+@EnableCircuitBreaker
 public class CoreServiceCategoryApplication {
 
 	public static void main(String[] args) {
