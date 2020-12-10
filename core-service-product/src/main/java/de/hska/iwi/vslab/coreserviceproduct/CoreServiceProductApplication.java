416d6a46ed5cc13f288bcd9afed57173b51d022e
Grymare <hendrik.diemer@gmail.com>
2020-12-10 10:44:47 +0100

    Added Hystrix Dashboard for Category and Product

--- core-service-product/src/main/java/de/hska/iwi/vslab/coreserviceproduct/CoreServiceProductApplication.java
+++ core-service-product/src/main/java/de/hska/iwi/vslab/coreserviceproduct/CoreServiceProductApplication.java
@@ -4,11 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableHystrix
+@EnableHystrixDashboard
+@Configuration
+@EnableCircuitBreaker
 public class CoreServiceProductApplication {
 
 	public static void main(String[] args) {
