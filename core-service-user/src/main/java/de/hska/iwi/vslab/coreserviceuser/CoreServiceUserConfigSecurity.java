9c7b48033de270bfccd518f80f9d18eed1e4f305
Christian Becker <christian.becker@inovex.de>
2021-03-24 09:33:06 +0100

    401  Unauthorized Access

--- core-service-user/src/main/java/de/hska/iwi/vslab/coreserviceuser/CoreServiceUserConfigSecurity.java
+++ core-service-user/src/main/java/de/hska/iwi/vslab/coreserviceuser/CoreServiceUserConfigSecurity.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -19,49 +20,12 @@ import org.springframework.security.oauth2.provider.token.store.InMemoryTokenSto
 @SuppressWarnings("deprecation")
 @Configuration
 @EnableAuthorizationServer
-public class CoreServiceUserConfig extends AuthorizationServerConfigurerAdapter {
-
-	@Autowired
-	private AuthenticationManager authenticationManager;
-
-	/*
-	@Override
-	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.authenticationManager(authenticationManager);
-	}
-
-	@Override
-	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-		security.checkTokenAccess("isAuthenticated()");
-	}
-	*/
-	@Override
-	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		// @formatter:off
-		clients.inMemory()
-		/*
-			.withClient("my-trusted-client")
-				.authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
-				.authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
-				.scopes("read", "write", "trust")
-				.resourceIds("oauth2-resource")
-				.accessTokenValiditySeconds(600)
-		.and()
-			.withClient("my-client-with-registered-redirect")
-				.authorizedGrantTypes("authorization_code")
-				.authorities("ROLE_CLIENT")
-				.scopes("read", "trust")
-				.resourceIds("oauth2-resource")
-				.redirectUris("http://anywhere?key=value")
-		.and()
-		*/
-			.withClient("my-client-with-secret")
-				.authorizedGrantTypes("client_credentials", "password")
-				.authorities("ROLE_CLIENT")
-				.scopes("read")
-				.resourceIds("oauth2-resource")
-				.secret("secretPassword");
-		// @formatter:on
-	}
+public class CoreServiceUserConfigSecurity extends WebSecurityConfigurerAdapter {	
+		
+		@Bean
+		@Override
+		public AuthenticationManager authenticationManagerBean() throws Exception {
+			return super.authenticationManagerBean();
+		}
 
 }
